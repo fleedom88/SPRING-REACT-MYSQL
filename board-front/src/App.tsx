b61ae7ac0cdb626cec7643a46d6ecc4e11008429
FREEDOM <fleedom88>
2024-04-08 23:11:05 +0900

    [frontend] Footer 레이아웃 작성 React-Router-Dom SPA작업

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,21 +1,51 @@
-import React, { useState } from 'react';
-
+import Footer from 'layouts/Footer';
 import './App.css';
-import BoardItem from 'components/BoardItem';
-import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
-import Top3Item from 'components/Top3Item';
-import CommentItem from 'components/CommentItem';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/inputBox';
+import { Route, Routes } from 'react-router-dom';
+import Main from 'views/Main';
+import Authentication from 'views/Authentication';
+import Search from 'views/Search';
+import User from 'views/User';
+import BoardDeatil from 'views/Board/Detail';
+import BoardWrite from 'views/Board/Write';
+import BoardUpdate from 'views/Board/Update';
+import Container from 'layouts/Container';
+import { MAIN_PATH } from 'constant';
+import { AUTH_PATH } from 'constant';
+import { SEARCH_PATH } from 'constant';
+import { USER_PATH } from 'constant';
+import { BOARD_PATH } from 'constant';
+import { BOARD_WRITE_PATH } from 'constant';
+import { BOARD_DETAIL_PATH } from 'constant';
+import { BOARD_UPDATE_PATH } from 'constant';
 
+
+//        component: Application 컴포넌트             //
 function App() {
 
-  const [value, setValue] = useState<string>('');
 
+//         render: Application 컴포넌트 렌더링            //
+//  description: 메인 화면        : '/' - Main//
+//  description:로그인+회원가입    : '/auth/ - Authentication  //
+//  description: 검색 화면        : '/search/:searchWord'  - Search  //
+//  description: 유저 페이지       : '/user/:emailEmail'  - User  //
+//  description: 게시물 상세보기   : '/board/detail/:boardNumber'  - BoardDetail //
+//  description: 게시물 작성하기   : '/board/write' - BoardWrite//
+//  description: 게시물 수정하기   : '/board/update/:boardNumber' - BoardUpdate //
   return (
-    <>
-      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요.' value={value} error={true} setValue={setValue} message='aaa'/>
-    </>
+    <Routes>
+        <Route element={<Container/>}>
+              <Route path={MAIN_PATH()} element={<Main/>} />
+              <Route path={AUTH_PATH()} element={<Authentication/>} />
+              <Route path={SEARCH_PATH(':searchWord')} element={<Search/>} />
+              <Route path={USER_PATH(':userEMail')} element={<User/>} />
+              <Route path={BOARD_PATH()}>
+                <Route path={BOARD_WRITE_PATH()} element={<BoardDeatil/>} />
+                <Route path={BOARD_DETAIL_PATH(':boardNumber')} element={<BoardWrite/>} />
+                <Route path={BOARD_UPDATE_PATH(':boardNumber')} element={<BoardUpdate/>} />
+              </Route>
+              <Route path='*' element={<h1>404 Not Found</h1>}/>
+        </Route>
+    </Routes>
   );
 }
 
