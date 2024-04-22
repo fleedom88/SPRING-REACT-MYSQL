b453750327823d4288c8828c682ee15a9b6f53f6
FREEDOM <fleedom88>
2024-04-22 23:04:42 +0900

    [frontend] 로그인 인풋박스 수정 ~29:17

--- board-front/src/components/inputBox/index.tsx
+++ board-front/src/components/inputBox/index.tsx
@@ -33,7 +33,7 @@ const InputBox = forwardRef<HTMLInputElement, Props>((props:Props, ref) =>{ //us
         setValue(value);
     };
 
-//          Key event handler: input 값 변경 이벤트 처리 함수           //
+//          event handler: input 키 이벤트 처리 함수           //
     const onKeyDownHandler=(event: KeyboardEvent<HTMLInputElement>)=>{
         if(!onKeyDown) return;
         onKeyDown(event);
@@ -46,15 +46,12 @@ const InputBox = forwardRef<HTMLInputElement, Props>((props:Props, ref) =>{ //us
             <div className='inputbox-label'>{label}</div>
             <div className={error ?'inputbox-container-error': 'inputbox-container'}>
                 {/* ref를 쓰는 이유는 엔터쳤을 때 다음요소로 넘어가기 위함 */}
-                <input ref={ref} type={type} className='input' placeholder={placeholder} value={value} onChange={onChangeHandler} onKeyDown={onKeyDown}/>
+                <input ref={ref} type={type} className='input' placeholder={placeholder} value={value} onChange={onChangeHandler} onKeyDown={onKeyDownHandler}/>
                 {onButtonClick !== undefined &&
-                    <div className='icon-button'>
+                    <div className='icon-button' onClick={onButtonClick}>
                         {icon !== undefined &&<div className={`icon ${icon}`}></div>}
                     </div>
                 }
-                <div className='icon-button' onClick={onButtonClick}>
-                    <div className='icon eye-light-off-icon'></div>
-                </div>
             </div>
             {message !== undefined && <div className='inputbox-message'>{message}</div>
             }            
--- board-front/src/views/Authentication/index.tsx
+++ board-front/src/views/Authentication/index.tsx
@@ -1,4 +1,4 @@
-import React, { KeyboardEvent, useRef, useState } from 'react';
+import React, { ChangeEvent, KeyboardEvent, useRef, useState } from 'react';
 import './style.css';
 import InputBox from 'components/inputBox';
 
@@ -50,14 +50,14 @@ const SignInCard = () => {
     }
   }
 
-  //       event handler: 이메일 인풋 키 다운 이벤트 처리 함수           //
+  //       event handler: 이메일 키 다운 이벤트 처리 함수           //
   const onEmailKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) =>{
     if(event.key !== 'Enter') return;
     if(!passwordRef.current) return;
     passwordRef.current.focus();    
   }
 
-  //       event handler: 이메일 인풋 키 다운 이벤트 처리 함수           //
+  //       event handler: 패스워드 키 다운 이벤트 처리 함수           //
   const onPasswordKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) =>{
     if(event.key !== 'Enter') return;
     onSignInButtonClickHandler();
@@ -93,10 +93,107 @@ const SignInCard = () => {
 
 //          component: sign up card 컴포넌트             //
 const SignUpCard = () =>{
+
+  //          state: 이메일 요소 참조 상태             //
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  //          state: 패스워드 요소 참조 상태             //
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+  //          state: 패스워드 체크 요소 참조 상태             //
+  const passwordCheckRef = useRef<HTMLInputElement | null>(null);
+  //          state: 페이지 번호 상태                   //
+  const [page, setPage] = useState<1 | 2> (1);
+  //          state: 이메일 상태                        //
+  const [email, setEmail] = useState<string>('');
+  //          state: 패스워드 상태                      //
+  const [password, setPassword] = useState<string>('');
+  //          state: 패스워드 확인 상태                  //
+  const [passwordCheck, setPasswordCheck] = useState<string>('');
+
+  //          state: 패스워드 타입 상태                  //
+  const [passwordType, setPasswordType] = useState<'text' | 'password'>('password');
+  //          state: 패스워드 체크 타입 상태                  //
+  const [passwordCheckType, setPasswordCheckType] = useState<'text' | 'password'>('password');
+  //          state: 이메일 에러 상태                  //
+  const [isEmailError, setEmailError] = useState<boolean>(false);
+  //          state: 패스워드 에러 상태                  //
+  const [isPasswordError, setPasswordError] = useState<boolean>(false);
+  //          state: 이메일 에러 메시지 상태                  //
+  const [emailErrorMessage, setEmailErrorMessage] = useState<string>('');
+  //          state: 패스워드 에러 메시지 상태                  //
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState<string>('');
+  //          state: 이메일 에러 메시지 상태                  //
+  const [passwordCheckErrorMessage, setPasswordCheckErrorMessage] = useState<string>('');
+  //          state: 패스워드 버튼 아이콘 상태                  //
+  const [passwordButtonIcon,setPasswordButtonIcon] = useState<'eye-light-off-icon' | 'eye-light-on-icon'>('eye-light-off-icon');
+  //          state: 패스워드 확인 아이콘 상태                  //
+  const [passwordCheckButtonIcon,setpasswordCheckButtonIcon] = useState<'eye-light-off-icon' | 'eye-light-on-icon'>('eye-light-off-icon');
   
+  
+  //          event Handler: 이메일 변경 이벤트 처리          //
+  const onEmailChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const {value} = event.target;
+    setEmail(value);
+  };
+  //          event Handler: 패스워드 변경 이벤트 처리          //
+  const onPasswordChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const {value} = event.target;
+    setPassword(value);
+  };
+  //          event Handler: 패스워드 확인 변경 이벤트 처리          //
+  const onPasswordCheckChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const {value} = event.target;
+    setPasswordCheck(value);
+  };
+  //          event Handler: 패스워드 확인 변경 이벤트 처리          //
+  const onPasswordButtonChangeHandler = () => {
+    if(passwordButtonIcon === 'eye-light-off-icon'){
+      setPasswordButtonIcon('eye-light-on-icon');
+      setPasswordType('text');
+    }
+    else{
+      setPasswordButtonIcon('eye-light-off-icon');
+      setPasswordType('password');
+    }
+  };
+  //          event Handler: 패스워드 확인 버튼 클릭 이벤트 처리          //
+  const onPasswordCheckButtonChangeHandler = () => {
+    if(passwordButtonIcon === 'eye-light-off-icon'){
+      setPasswordButtonIcon('eye-light-on-icon');
+      setPasswordType('text');
+    }
+    else{
+      setPasswordButtonIcon('eye-light-off-icon');
+      setPasswordType('password');
+    }
+  };
+  //          event Handler: 이메일 키 다운 이벤트 처리          //
+  const onEmailKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) =>{
+    if(event.key !== 'Enter') return;
+    if(!passwordRef.current) return;
+    passwordRef.current.focus();
+  }
+
   //          render: sign up card 컴포넌트             //
   return (
-    <div className='auth-card'></div>
+    <div className='auth-card'>
+      <div className='auth-card-box'>
+      <div className='auth-card-top'>
+        <div className='auth-card-title-box'>
+          <div className='auth-card-title'>{'회원가입'}</div>
+          <div className='auth-card-page'>{`${page}/2`}</div>
+        </div>
+        <InputBox ref={emailRef} type='text' label='이메일 주소*' placeholder='이메일 주소를 입력해주세요.' value={email} onChange={onEmailChangeHandler} error={isEmailError} message={emailErrorMessage} /> 
+        <InputBox ref={passwordRef} type={passwordType} label='비밀번호*' placeholder='비밀번호를 입력해주세요.' value={password}  onChange={onPasswordChangeHandler} error={isPasswordError} message={passwordErrorMessage} icon={passwordButtonIcon} /> 
+        <InputBox ref={passwordCheckRef} type={passwordCheckType} label='비밀번호 확인*' placeholder='비밀번호를 다시 입력해주세요.' value={passwordCheck} onChange={onPasswordCheckChangeHandler} message={passwordCheckErrorMessage} icon={passwordCheckButtonIcon} /> 
+      </div>
+      <div className='auth-card-bottom'>
+        <div className='black-large-full-button'>{'다음 단계'}</div>
+        <div className='auth-description-box'>
+          <div className='auth-description'>{'이미 계정이 있으신가요?'}<span className='auth-description-link'>{'로그인'}</span></div>
+        </div>
+      </div>
+      </div>
+    </div>
   );
 };
 
